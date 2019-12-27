f03d45fba02ccf64f07f00a45280ff391adbf757
gaoshiwei <1020431880@qq.com>
2019-12-27 17:18:39 +0800

    no message

--- env/config.js
+++ env/config.js
@@ -1,26 +1,31 @@
+// 链接后台的环境
 const ENV = "local"
-
+// 链接配置
 const config = {
+  // 本地环境
   local: {
     API_URL: "http://localhost:5000",
     APPID: "wxbef61728f6bee6b0",
     APP_SECRET: "643cc852782efefcf518ea9fb9b10a48"
   },
+  // 开发环境
   dev: {
     API_URL: "http://localhost:8080",
     APPID: "wxbef61728f6bee6b0",
     APP_SECRET: "643cc852782efefcf518ea9fb9b10a48"
   },
+  // 预发布环境
   pre: {
     API_URL: "http://localhost:8080",
     APPID: "wxbef61728f6bee6b0",
     APP_SECRET: "643cc852782efefcf518ea9fb9b10a48"
   },
+  // 线上环境
   pro: {
     API_URL: "http://localhost:8080",
     APPID: "wxbef61728f6bee6b0",
     APP_SECRET: "643cc852782efefcf518ea9fb9b10a48"
   }
 }
-
+// 导出配置
 export default config[ENV]
